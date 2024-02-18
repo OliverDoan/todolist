046933d3079ccb2d3ab4d931fe4c4cfc7bba308a
OliverDoan <doanvietthuan1005@gmail.com>
2024-02-18 23:57:17 +0700

    fix: variable not use

--- src/redux/slices/filterStatusSlice.ts
+++ src/redux/slices/filterStatusSlice.ts
@@ -9,7 +9,7 @@ export const filterStatusSlice = createSlice({
   name: 'filterStatus',
   initialState,
   reducers: {
-    filterStatusTodo: (state, action) => {
+    filterStatusTodo: (_state, action) => {
       return action.payload
     }
   }
